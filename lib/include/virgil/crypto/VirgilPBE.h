3bff8c6649308815a86927abdc2c2144eb637d4d
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2015-01-14 20:29:39 +0200

    Lib: Added Password-Based Cryptography. Now PKCS#5 and PKCS#12 are partially supported.

---
+++ lib/include/virgil/crypto/VirgilPBE.h
@@ -0,0 +1,167 @@
+/**
+ * Copyright (C) 2014 Virgil Security Inc.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *     (3) Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef VIRGIL_CRYPTO_VIRGIL_PBE_H
+#define VIRGIL_CRYPTO_VIRGIL_PBE_H
+
+#include <cstddef>
+#include <virgil/VirgilByteArray.h>
+#include <virgil/crypto/VirgilAsn1Compatible.h>
+
+namespace virgil { namespace crypto {
+
+/**
+ * @name Forward declarations
+ */
+///@{
+class VirgilPBEImpl;
+///@}
+
+/**
+ * @brief Provides Password-Based Cryptography. Now PKCS#5 and PKCS#12 are partially supported.
+ */
+class VirgilPBE : public VirgilAsn1Compatible {
+public:
+    /**
+     * @name Constants
+     */
+    ///@{
+    enum {
+        kIterationCountMin = 1024
+    };
+    ///@}
+public:
+    /**
+     * @name Creation methods
+     * @brief Object creation with specific hash function.
+     */
+    ///@{
+    /**
+     * @brief Create object with PKCS#5 parameters for PBE encryption or decryption.
+     * @note Recommended PKCS#5 parameters are set.
+     */
+    static VirgilPBE pkcs5(const VirgilByteArray& salt, size_t iterationCount = kIterationCountMin);
+    /**
+     * @brief Create object with PKCS#12 parameters for PBE encryption or decryption.
+     * @note Recommended PKCS#12 parameters are set.
+     */
+    static VirgilPBE pkcs12(const VirgilByteArray& salt, size_t iterationCount = kIterationCountMin);
+    ///@}
+    /**
+     * @name Constructor / Destructor
+     */
+    ///@{
+    /**
+     * @brief Create object with undefined algorithm.
+     * @warning SHOULD be used in conjunction with VirgilAsn1Compatible interface,
+     *     i.e. VirgilPBE pbe = VirgilPBE().fromAsn1(asn1);
+     */
+    VirgilPBE();
+    /**
+     * @brief Polymorphic destructor.
+     */
+    virtual ~VirgilPBE() throw();
+    ///@}
+    /**
+     * @name Encryption / Decryption
+     */
+    ///@{
+    /**
+     * @brief Encrypt data with given password.
+     * @param data - data to encrypt.
+     * @param pwd - password to use when generating key.
+     * @return Encrypted data.
+     */
+    VirgilByteArray encrypt(const VirgilByteArray& data, const VirgilByteArray& pwd) const;
+    /**
+     * @brief Decrypt data with given password.
+     * @param data - data to decrypt.
+     * @param pwd - password to use when generating key.
+     * @return Decrypted data.
+     */
+    VirgilByteArray decrypt(const VirgilByteArray& data, const VirgilByteArray& pwd) const;
+    ///@}
+    /**
+     * @name Copy constructor / assignment operator
+     * @warning Copy constructor and assignment operator create copy of the object as it was created
+     *          by on of the creation methods. All changes in the internal state,
+     *          that was made after creation, are not copied!
+     */
+    ///@{
+    VirgilPBE(const VirgilPBE& other);
+    VirgilPBE& operator=(const VirgilPBE& rhs);
+    ///@}
+    /**
+     * @name VirgilAsn1Compatible implementation
+     */
+    ///@{
+    virtual VirgilByteArray toAsn1() const;
+    virtual void fromAsn1(const VirgilByteArray& asn1);
+    ///@}
+private:
+    /**
+     * @brief Creates and initialize PBKDF with specified type.
+     * @warning Constructor CAN NOT be used directly, use one of factory methods to create apropriate cipher.
+     */
+    explicit VirgilPBE(int type, const VirgilByteArray& salt, size_t iterationCount);
+    /**
+     * @brief If internal state is not initialized with specific algorithm exception will be thrown.
+     */
+    void checkState() const;
+    /**
+     * @brief Encrypt or decrypt data depend on the mode.
+     */
+    VirgilByteArray process(const VirgilByteArray& data, const VirgilByteArray& pwd, int mode) const;
+    /**
+     * @brief Trim trailing zeros.
+     */
+     void trimTrailingZeros(VirgilByteArray& data) const;
+    /**
+     * @brief Trim padding.
+     */
+     void trimPadding(VirgilByteArray& data) const;
+    /**
+     * @brief Trim PKCS#7 padding.
+     * @return true if PKCS#7 padding was detected and successfully trimmed.
+     */
+     bool trimPKCS7Padding(VirgilByteArray& data) const;
+private:
+    VirgilPBEImpl *impl_;
+};
+
+}}
+
+#endif /* VIRGIL_CRYPTO_VIRGIL_PBE_H */
---
+++ lib/src/VirgilPBE.cxx
@@ -0,0 +1,363 @@
+/**
+ * Copyright (C) 2014 Virgil Security Inc.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *     (3) Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <virgil/crypto/VirgilPBE.h>
+using virgil::crypto::VirgilPBE;
+
+#include <polarssl/asn1.h>
+#include <polarssl/oid.h>
+#include <polarssl/pkcs5.h>
+#include <polarssl/pkcs12.h>
+#include <polarssl/cipher.h>
+#include <polarssl/md.h>
+
+#include <map>
+#include <string>
+#include <algorithm>
+
+#include <cstring>
+
+#include <virgil/VirgilByteArray.h>
+using virgil::VirgilByteArray;
+
+#include <virgil/crypto/PolarsslException.h>
+
+#include <virgil/crypto/VirgilCryptoException.h>
+using virgil::crypto::VirgilCryptoException;
+
+#include <virgil/crypto/VirgilAsn1Compatible.h>
+using virgil::crypto::VirgilAsn1Compatible;
+
+#include <virgil/crypto/VirgilAsn1Reader.h>
+using virgil::crypto::VirgilAsn1Reader;
+
+#include <virgil/crypto/VirgilAsn1Writer.h>
+using virgil::crypto::VirgilAsn1Writer;
+
+#include <virgil/crypto/VirgilRandom.h>
+using virgil::crypto::VirgilRandom;
+
+typedef enum {
+    VIRGIL_PBE_NONE = 0,
+    VIRGIL_PBE_PKCS5,
+    VIRGIL_PBE_PKCS12,
+    VIRGIL_PBE_PKCS12_SHA1_RC4_128
+} VirgilPBEType;
+
+namespace virgil { namespace crypto {
+
+class VirgilPBEImpl {
+public:
+    VirgilPBEType type;
+    VirgilRandom random;
+    VirgilByteArray algId;
+    asn1_buf pbeAlgOID;
+    asn1_buf pbeParams;
+    md_type_t mdType;
+    cipher_type_t cipherType;
+public:
+    VirgilPBEImpl() : type(VIRGIL_PBE_NONE),
+            random(VIRGIL_BYTE_ARRAY_FROM_STD_STRING(std::string("com.virgilsecurity.VirgilPBE"))) {
+    }
+
+    explicit VirgilPBEImpl(VirgilPBEType pbeType, const VirgilByteArray& salt, size_t iterationCount) : type(pbeType),
+            random(VIRGIL_BYTE_ARRAY_FROM_STD_STRING(std::string("com.virgilsecurity.VirgilPBE"))) {
+        const size_t adjustedIterationCount =
+                iterationCount < VirgilPBE::kIterationCountMin ? VirgilPBE::kIterationCountMin : iterationCount;
+        switch (pbeType) {
+            case VIRGIL_PBE_PKCS5:
+                init_(buildAlgIdPKCS5(salt, adjustedIterationCount));
+                break;
+            case VIRGIL_PBE_PKCS12:
+                init_(buildAlgIdPKCS12(salt, adjustedIterationCount));
+                break;
+            default:
+                throw VirgilCryptoException("VirgilPBE: Given algorithm is not supported.");
+        }
+
+    }
+    explicit VirgilPBEImpl(const VirgilByteArray& pbeAlgId) : type(VIRGIL_PBE_NONE),
+            random(VIRGIL_BYTE_ARRAY_FROM_STD_STRING(std::string("com.virgilsecurity.VirgilPBE"))) {
+        init_(pbeAlgId);
+    }
+private:
+    /**
+     * @brief Parse given PBE algorithm identifier and stores parsed data as local object state.
+     * @note Algorithm identifier is distrubuted in ASN.1 DER encoded structure:
+     *     AlgorithmIdentifier ::= SEQUENCE {
+     *         algorithm OBJECT IDENTIFIER,
+     *         parameters ANY DEFINED BY algorithm OPTIONAL }
+     * @throw VirgilCryptoException if algorithm identifier is not supported or ASN.1 structure is corrupted.
+     */
+    void init_(const VirgilByteArray& pbeAlgId) {
+        size_t len;
+        unsigned char *p, *end;
+
+        // Initial init
+        type = VIRGIL_PBE_NONE;
+        algId = pbeAlgId;
+        mdType = POLARSSL_MD_NONE;
+        cipherType = POLARSSL_CIPHER_NONE;
+        memset (&pbeAlgOID, 0x00, sizeof(pbeAlgOID));
+        memset (&pbeParams, 0x00, sizeof(pbeParams));
+
+        // Parse ASN.1
+        p = const_cast<unsigned char *>(algId.data());
+        end = p + algId.size();
+
+        POLARSSL_ERROR_HANDLER(
+            ::asn1_get_alg(&p, end, &pbeAlgOID, &pbeParams)
+        );
+
+        if (oid_get_pkcs12_pbe_alg(&pbeAlgOID, &mdType, &cipherType) == 0) {
+            type = VIRGIL_PBE_PKCS12;
+        } else if (OID_CMP(OID_PKCS12_PBE_SHA1_RC4_128, &pbeAlgOID)) {
+            type = VIRGIL_PBE_PKCS12_SHA1_RC4_128;
+        } else if (OID_CMP(OID_PKCS5_PBES2, &pbeAlgOID)) {
+            type = VIRGIL_PBE_PKCS5;
+        } else {
+            throw VirgilCryptoException("VirgilPBE: Given algorithm is not supported.");
+        }
+    }
+
+    /**
+     * pbes2 ::= SEQUENCE {
+     *   id-PBES2 OBJECT IDENTIFIER ::= {pkcs-5 13},
+     *   PBES2-params ::= SEQUENCE {
+     *      keyDerivationFunc AlgorithmIdentifier {{PBES2-KDFs}},
+     *      encryptionScheme AlgorithmIdentifier {{PBES2-Encs}} }
+     * }
+     *
+     * PBES2-KDFs ALGORITHM-IDENTIFIER ::=
+     *    { {PBKDF2-params IDENTIFIED BY id-PBKDF2}, ... }
+     *
+     * PBES2-Encs ALGORITHM-IDENTIFIER ::= { ... }
+     */
+    VirgilByteArray buildAlgIdPKCS5(const VirgilByteArray& salt, size_t iterationCount) {
+        VirgilAsn1Writer asn1Writer;
+        const char *oid = 0;
+        size_t oidLen;
+        // Write PBES2-params
+        size_t pbesLen = 0;
+        {
+            // Write PBES2-Enc
+            cipherType = POLARSSL_CIPHER_DES_EDE3_CBC;
+            POLARSSL_ERROR_HANDLER(
+                ::oid_get_oid_by_cipher_alg(cipherType, &oid, &oidLen)
+            );
+            const cipher_info_t *cipherInfo = cipher_info_from_type(cipherType);
+            if (cipherInfo == 0) {
+                throw VirgilCryptoException("VirgilPBE: Given cipher is not supported.");
+            }
+            size_t encLen = 0;
+            encLen += asn1Writer.writeOctetString(random.randomize(cipherInfo->iv_size));
+            encLen += asn1Writer.writeOID(std::string(oid, oidLen));
+            encLen += asn1Writer.writeSequence(encLen);
+            // Write PBES2-KDF
+            size_t kdfLen = 0;
+            kdfLen += asn1Writer.writeInteger(iterationCount);
+            kdfLen += asn1Writer.writeOctetString(salt);
+            kdfLen += asn1Writer.writeSequence(kdfLen);
+            kdfLen += asn1Writer.writeOID(std::string(OID_PKCS5_PBKDF2, OID_SIZE(OID_PKCS5_PBKDF2)));
+            kdfLen += asn1Writer.writeSequence(kdfLen);
+
+            pbesLen += encLen + kdfLen;
+            pbesLen += asn1Writer.writeSequence(pbesLen);
+        }
+        // Write id-PBES2 OBJECT IDENTIFIER
+        pbesLen += asn1Writer.writeOID(std::string(OID_PKCS5_PBES2, OID_SIZE(OID_PKCS5_PBES2)));
+        asn1Writer.writeSequence(pbesLen);
+
+        return asn1Writer.finish();
+    }
+
+    /**
+     * pkcs-12Pbe ::= SEQUENCE {
+     *   pkcs-12PbeId OBJECT IDENTIFIER ::= {{pkcs-12PbeIds}},
+     *   pkcs-12PbeParams ::= SEQUENCE {
+     *       salt        OCTET STRING,
+     *       iterations  INTEGER
+     *   }
+     * }
+     */
+    VirgilByteArray buildAlgIdPKCS12(const VirgilByteArray& salt, size_t iterationCount) {
+        VirgilAsn1Writer asn1Writer;
+        const char *oid = 0;
+        size_t oidLen;
+        // Write PBE-params
+        size_t pbesLen = 0;
+        pbesLen += asn1Writer.writeInteger(iterationCount);
+        pbesLen += asn1Writer.writeOctetString(salt);
+        pbesLen += asn1Writer.writeSequence(pbesLen);
+        // Write id-PBE OBJECT IDENTIFIER
+        pbesLen += asn1Writer.writeOID(
+                std::string(OID_PKCS12_PBE_SHA1_DES3_EDE_CBC, OID_SIZE(OID_PKCS12_PBE_SHA1_DES3_EDE_CBC)));
+        asn1Writer.writeSequence(pbesLen);
+
+        return asn1Writer.finish();
+    }
+};
+
+}}
+
+VirgilPBE VirgilPBE::pkcs5(const VirgilByteArray& salt, size_t iterationCount) {
+    return VirgilPBE(VIRGIL_PBE_PKCS5, salt, iterationCount);
+}
+
+VirgilPBE VirgilPBE::pkcs12(const VirgilByteArray& salt, size_t iterationCount) {
+    return VirgilPBE(VIRGIL_PBE_PKCS12, salt, iterationCount);
+}
+
+VirgilPBE::VirgilPBE() : impl_(new VirgilPBEImpl()) {
+}
+
+VirgilPBE::VirgilPBE(int type, const VirgilByteArray& salt, size_t iterationCount)
+        : impl_(new VirgilPBEImpl(static_cast<VirgilPBEType>(type), salt, iterationCount)) {
+}
+
+VirgilPBE::~VirgilPBE() throw() {
+    if (impl_) {
+        delete impl_;
+        impl_ = 0;
+    }
+}
+
+VirgilPBE::VirgilPBE(const VirgilPBE& other) : impl_(new VirgilPBEImpl(other.impl_->algId)) {
+}
+
+VirgilPBE& VirgilPBE::operator=(const VirgilPBE& rhs) {
+    if (this == &rhs) {
+        return *this;
+    }
+    VirgilPBEImpl *newImpl = new VirgilPBEImpl(rhs.impl_->algId);
+    if (impl_) {
+        delete impl_;
+    }
+    impl_ = newImpl;
+    return *this;
+}
+
+VirgilByteArray VirgilPBE::encrypt(const VirgilByteArray& data, const VirgilByteArray& pwd) const {
+    int mode = (impl_->type == VIRGIL_PBE_PKCS5) ? PKCS5_ENCRYPT : PKCS12_PBE_ENCRYPT;
+    return process(data, pwd, mode);
+}
+
+VirgilByteArray VirgilPBE::decrypt(const VirgilByteArray& data, const VirgilByteArray& pwd) const {
+    int mode = (impl_->type == VIRGIL_PBE_PKCS5) ? PKCS5_DECRYPT : PKCS12_PBE_DECRYPT;
+    return process(data, pwd, mode);
+}
+
+VirgilByteArray VirgilPBE::process(const VirgilByteArray& data, const VirgilByteArray& pwd, int mode) const {
+    checkState();
+    const size_t CIPHER_BLOCK_SIZE_MAX = 64;
+    VirgilByteArray output(data.size() + CIPHER_BLOCK_SIZE_MAX);
+    asn1_buf pbeParams = impl_->pbeParams;
+    switch (impl_->type) {
+        case VIRGIL_PBE_PKCS5:
+            POLARSSL_ERROR_HANDLER(
+                ::pkcs5_pbes2(&pbeParams, mode,
+                        VIRGIL_BYTE_ARRAY_TO_PTR_AND_LEN(pwd),
+                        VIRGIL_BYTE_ARRAY_TO_PTR_AND_LEN(data),
+                        output.data())
+            );
+            break;
+        case VIRGIL_PBE_PKCS12:
+            POLARSSL_ERROR_HANDLER(
+                pkcs12_pbe(&pbeParams, mode,
+                        impl_->cipherType, impl_->mdType,
+                        VIRGIL_BYTE_ARRAY_TO_PTR_AND_LEN(pwd),
+                        VIRGIL_BYTE_ARRAY_TO_PTR_AND_LEN(data),
+                        output.data())
+            );
+            break;
+        case VIRGIL_PBE_PKCS12_SHA1_RC4_128:
+            POLARSSL_ERROR_HANDLER(
+                pkcs12_pbe_sha1_rc4_128(&pbeParams, mode,
+                        VIRGIL_BYTE_ARRAY_TO_PTR_AND_LEN(pwd),
+                        VIRGIL_BYTE_ARRAY_TO_PTR_AND_LEN(data),
+                        output.data())
+            );
+            break;
+        default:
+            throw VirgilCryptoException("VirgilPBE: Given algorithm is not supported.");
+    }
+    trimTrailingZeros(output);
+    // Trim trailing padding. Only in decryption mode.
+    if ((impl_->type == VIRGIL_PBE_PKCS5 && mode == PKCS5_DECRYPT) ||
+            (impl_->type != VIRGIL_PBE_PKCS5 && mode == PKCS12_PBE_DECRYPT)) {
+        trimPadding(output);
+    }
+    return output;
+}
+
+
+void VirgilPBE::trimTrailingZeros(VirgilByteArray& data) const {
+    while (data.size() > 0 && data.back() == 0x00) {
+        data.pop_back();
+    }
+}
+
+void VirgilPBE::trimPadding(VirgilByteArray& data) const {
+    trimPKCS7Padding(data);
+}
+
+bool VirgilPBE::trimPKCS7Padding(VirgilByteArray& data) const {
+    int paddingCnt = std::count(data.rbegin(), data.rend(), data.back());
+    if (paddingCnt == (int)data.back()) {
+        data.resize(data.size() - paddingCnt);
+        return true;
+    }
+    return false;
+}
+
+void VirgilPBE::checkState() const {
+    if (impl_->type == VIRGIL_PBE_NONE) {
+        throw VirgilCryptoException(std::string("VirgilPBE: object has undefined algorithm.") +
+                std::string(" Use one of the factory methods or method 'fromAsn1' to define hash algorithm."));
+    }
+}
+
+VirgilByteArray VirgilPBE::toAsn1() const {
+    checkState();
+    return impl_->algId;
+}
+
+void VirgilPBE::fromAsn1(const VirgilByteArray& asn1) {
+    if (impl_) {
+        delete impl_;
+    }
+    impl_ = new VirgilPBEImpl(asn1);
+}

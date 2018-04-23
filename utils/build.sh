VirgilSecurity/virgil-crypto

commit a7581aebe6ac137f139dd197f254a8e8fcbcd59e

[BUGFIX][PYTHIA] Link net_mono with library relic and library pythia

utils/build.sh
@@ -186,6 +186,8 @@ function make_fat_library {
 
     LIBMBEDTLS="libmbedcrypto.a"
     LIBED25519="libed25519.a"
+    LIBRELIC="librelic_s.a"
+    LIBPYTHIA="libpythia.a"
     LIBVIRGIL="libvirgil_crypto.a"
     if [ ! -z "${WRAPPER_NAME}" ]; then
         LIBVIRGIL_WRAPPER="virgil_crypto_${WRAPPER_NAME}.a"
@@ -200,6 +202,12 @@ function make_fat_library {
     # Find all archs of library ed25519
     LIBED25519_LIBS=$(find "${INDIR}" -name "${LIBED25519}" | tr '\n' ' ')
 
+    # Find all archs of library relic
+    LIBRELIC_LIBS=$(find "${INDIR}" -name "${LIBRELIC}" | tr '\n' ' ')
+
+    # Find all archs of library pythia
+    LIBPYTHIA_LIBS=$(find "${INDIR}" -name "${LIBPYTHIA}" | tr '\n' ' ')
+
     # Find all archs of library Virgil Crypto
     LIBVIRGIL_LIBS=$(find "${INDIR}" -name "${LIBVIRGIL}" | tr '\n' ' ')
 
@@ -210,7 +218,10 @@ function make_fat_library {
 
     xcrun lipo -create ${LIBMBEDTLS_LIBS} -output "$OUTDIR/$LIBMBEDTLS"
     xcrun lipo -create ${LIBED25519_LIBS} -output "$OUTDIR/$LIBED25519"
+    xcrun lipo -create ${LIBRELIC_LIBS} -output "$OUTDIR/$LIBRELIC"
+    xcrun lipo -create ${LIBPYTHIA_LIBS} -output "$OUTDIR/$LIBPYTHIA"
     xcrun lipo -create ${LIBVIRGIL_LIBS} -output "$OUTDIR/$LIBVIRGIL"
+
     if [ ! -z "${LIBVIRGIL_WRAPPER_LIBS}" ]; then
         LIBVIRGIL_WRAPPER_FAT="$OUTDIR/$LIBVIRGIL_WRAPPER"
         xcrun lipo -create ${LIBVIRGIL_WRAPPER_LIBS} -output "$LIBVIRGIL_WRAPPER_FAT"
@@ -218,12 +229,15 @@ function make_fat_library {
 
     # Merge several static libraries in one static library which will actually be framework
     xcrun libtool -static -o "$OUTDIR/$LIB_FAT_NAME" \
-            "$OUTDIR/$LIBMBEDTLS" "$OUTDIR/$LIBED25519" "$OUTDIR/$LIBVIRGIL" "$LIBVIRGIL_WRAPPER_FAT"
+            "$OUTDIR/$LIBMBEDTLS" "$OUTDIR/$LIBED25519" "$OUTDIR/$LIBRELIC" \
+            "$OUTDIR/$LIBPYTHIA" "$OUTDIR/$LIBVIRGIL" "$LIBVIRGIL_WRAPPER_FAT"
 
     # Cleanup
     rm -f "$OUTDIR/$LIBMBEDTLS"
     rm -f "$OUTDIR/$LIBED25519"
     rm -f "$OUTDIR/$LIBVIRGIL"
+    rm -f "$OUTDIR/$LIBRELIC"
+    rm -f "$OUTDIR/$LIBPYTHIA"
     if [ -f "${LIBVIRGIL_WRAPPER_FAT}" ]; then
         rm -f "${LIBVIRGIL_WRAPPER_FAT}"
     fi

commit 0a62df5f87e14ca23833102e5773627f579ae656

[BUGFIX][PYTHIA] Fix broken build for Apple platforms that are build without Pythia
Bug was introduced in the commit: a7581aebe6ac137f139dd197f254a8e8fcbcd59e

utils/build.sh
@@ -218,9 +218,17 @@ function make_fat_library {
 
     xcrun lipo -create ${LIBMBEDTLS_LIBS} -output "$OUTDIR/$LIBMBEDTLS"
     xcrun lipo -create ${LIBED25519_LIBS} -output "$OUTDIR/$LIBED25519"
-    xcrun lipo -create ${LIBRELIC_LIBS} -output "$OUTDIR/$LIBRELIC"
-    xcrun lipo -create ${LIBPYTHIA_LIBS} -output "$OUTDIR/$LIBPYTHIA"
     xcrun lipo -create ${LIBVIRGIL_LIBS} -output "$OUTDIR/$LIBVIRGIL"
+
+    if [ ! -z "${LIBRELIC_LIBS}" ]; then
+        LIBRELIC_FAT="$OUTDIR/$LIBRELIC"
+        xcrun lipo -create ${LIBRELIC_LIBS} -output "${LIBRELIC_FAT}"
+    fi
+
+    if [ ! -z "${LIBPYTHIA_LIBS}" ]; then
+        LIBPYTHIA_FAT="$OUTDIR/$LIBPYTHIA"
+        xcrun lipo -create ${LIBPYTHIA_LIBS} -output "${LIBPYTHIA_FAT}"
+    fi
 
     if [ ! -z "${LIBVIRGIL_WRAPPER_LIBS}" ]; then
         LIBVIRGIL_WRAPPER_FAT="$OUTDIR/$LIBVIRGIL_WRAPPER"
@@ -230,7 +238,7 @@ function make_fat_library {
     # Merge several static libraries in one static library which will actually be framework
     xcrun libtool -static -o "$OUTDIR/$LIB_FAT_NAME" \
             "$OUTDIR/$LIBMBEDTLS" "$OUTDIR/$LIBED25519" "$OUTDIR/$LIBRELIC" \
-            "$OUTDIR/$LIBPYTHIA" "$OUTDIR/$LIBVIRGIL" "$LIBVIRGIL_WRAPPER_FAT"
+            "${LIBPYTHIA_FAT}" "${LIBRELIC_FAT}" "${LIBVIRGIL_WRAPPER_FAT}"
 
     # Cleanup
     rm -f "$OUTDIR/$LIBMBEDTLS"
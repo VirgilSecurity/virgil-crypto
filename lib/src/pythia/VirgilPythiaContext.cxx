fb2bbb03de903c0d12a6e8300edbc1b7c2887016
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-06-13 18:30:54 +0300

    [WIP] Print lifecycle of the class 'VirgilPythiaContext' for debug purposes
    !!! This commit MUST be reverted.

--- lib/src/pythia/VirgilPythiaContext.cxx
+++ lib/src/pythia/VirgilPythiaContext.cxx
@@ -55,6 +55,11 @@
 #include <thread>
 #include <tinyformat/tinyformat.h>
 
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+
 using virgil::crypto::make_error;
 using virgil::crypto::foundation::internal::mbedtls_context;
 using virgil::crypto::pythia::pythia_handler;
@@ -76,39 +81,84 @@ static void random_handler(uint8_t* out, int out_len, void*) {
     pythia_handler(mbedtls_ctr_drbg_random(g_rng_ctx.get(), out, out_len));
 }
 
+static std::string get_time() {
+    std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+    auto duration = now.time_since_epoch();
+
+    typedef std::chrono::duration<int, std::ratio_multiply<std::chrono::hours::period, std::ratio<8>>::type> Days; /* UTC: +8:00 */
+
+    Days days = std::chrono::duration_cast<Days>(duration);
+    duration -= days;
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(duration);
+    duration -= hours;
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration);
+    duration -= minutes;
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
+    duration -= seconds;
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    duration -= milliseconds;
+    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+    duration -= microseconds;
+    auto nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+
+    std::ostringstream sstream;
+    sstream   << std::setw(2) << std::setfill('0')<< hours.count() << ":"
+              << std::setw(2) << std::setfill('0')<< minutes.count() << ":"
+              << std::setw(2) << std::setfill('0')<< seconds.count() << ":"
+              << std::setw(3) << std::setfill('0')<< milliseconds.count() << ":"
+              << std::setw(3) << std::setfill('0')<< microseconds.count();
+    return sstream.str();
+}
+
 namespace internal {
 
 class PythiaContext {
 public:
     PythiaContext() {
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - started" << std::endl;
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - before RNG init" << std::endl;
         constexpr const char pers[] = "VirgilPythiaContext";
         g_rng_ctx.setup(mbedtls_entropy_func, g_entropy_ctx.get(), pers);
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - after RNG init" << std::endl;
 
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - before lock" << std::endl;
         std::lock_guard<std::mutex> lock_guard(g_instances_mutex);
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - after lock" << std::endl;
         if (g_instances++ > 0) {
+            std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - g_instances > 0" << std::endl;
             return;
         }
+
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - before global pythia init MUST be ONCE" << std::endl;
 
         pythia_init_args_t init_args;
         init_args.callback = random_handler;
         init_args.args = NULL;
 
         pythia_handler(pythia_init(&init_args));
+
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::PythiaContext() - after global pythia init MUST be ONCE" << std::endl;
     }
 
     ~PythiaContext() noexcept {
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::~PythiaContext() - before lock" << std::endl;
         std::lock_guard<std::mutex> lock_guard(g_instances_mutex);
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::~PythiaContext() - after lock" << std::endl;
         if (--g_instances > 0) {
+            std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::~PythiaContext() - g_instances > 0" << std::endl;
             return;
         }
 
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::~PythiaContext() - before global pythia deinit MUST be ONCE" << std::endl;
         pythia_deinit();
+        std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": PythiaContext::~PythiaContext() - after global pythia deinit MUST be ONCE" << std::endl;
     }
 };
 
 } // namespace internal
 
 VirgilPythiaContext::VirgilPythiaContext() {
+    std::cout << get_time() << "  :  Thread " << std::this_thread::get_id() << ": VirgilPythiaContext::VirgilPythiaContext() - started" << std::endl;
     //  Need to call ctor on a thread creation and dtor on thread exit
     static VIRGIL_THREAD_LOCAL internal::PythiaContext pythiaContext;
 }

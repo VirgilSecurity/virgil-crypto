539139947ffd6f530fa0cdc3c6f5f682be6ecc98
Travis CI documentation builder <sergey.seroshtan@gmail.com>
2019-05-14 15:11:06 +0000

    Automated documentation build for changeset 283ec81dfe70ab084748a04223312c255d0a3c49.

---
+++ v2.6/search/searchdata.js
@@ -0,0 +1,45 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijkmnoprstuvwz~",
+  1: "v",
+  2: "v",
+  3: "v",
+  4: "abcdefghijkmnoprstuvwz~",
+  5: "cekpr",
+  6: "av",
+  7: "aptv",
+  8: "aefikmnoprsuz",
+  9: "cehkp",
+  10: "v"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "groups",
+  10: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs",
+  7: "Enumerations",
+  8: "Enumerator",
+  9: "Modules",
+  10: "Pages"
+};
+
